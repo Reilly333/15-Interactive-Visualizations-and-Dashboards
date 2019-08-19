a18b25f775953f17d7d50dd6db65633515cdeb00
reilly333 <rbecker2@uncc.edu>
2019-08-19 12:36:41 -0400

    commit3

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,7 +4,7 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
   var md_url = `/metadata/${sample}`;
-  d3.json(md_url).then(function (response) {
+  d3.json(md_url).then(function (data) {
     // Use d3 to select the panel with id of `#sample-metadata`
      var metadata = d3.select("#sample-metadata");
 
@@ -21,54 +21,55 @@ function buildMetadata(sample) {
         })
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+	})
 }
 
 function buildCharts(sample) {
-
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var sd_url = `/samples/${sample}`;
+  var sd_url= `/samples/${sample}`;
  
- d3.json(sd_url).then((function(response) {
+ d3.json(sd_url).then((function(data) {
     // @TODO: Build a Bubble Chart using the sample data
-    var bc_ids = response.otu_ids;
-    var bc_labels = response.otu_labels;
-    var chart_values = response.sample_values;
+    var bc_ids = data.otu_ids;
+    var bc_labels = data.otu_labels;
+    var chart_values = data.sample_values;
 
-      var bc_data = {
+      var bc_data =  {
       mode: 'markers',
       x: bc_ids,
       y: chart_values,
       text: bc_labels,
       marker: {color: bc_ids, colorscale: "Earth", size: chart_values}
     };
+    var data1 = [bc_data];
     // @TODO: Build a Pie Chart
 
      var chart_layout = {
-        title: 'Sample data breakdown',
+        title: 'Bacteria Distribution Breakdown',
         showlegend: true,
         height: 600,
         width : 600
       };
-  Plotly.plot("bubble", bc_data, chart_layout);
+  Plotly.newPlot("bubble", data1, chart_layout);
 
-});
+	}))
+};
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-d3.json(sd_url).then((function(response) {
-    var ten_sample_ids = response.otu_ids.slice(0,10);
-    var ten_sample_labels = response.otu_labels.slice(0,10);
-    var ten_sample_values = response.sample_values.slice(0,10);
+  d3.json('/samples/${sample}').then(function(data) {
+    var ten_sample_ids = data.otu_ids.slice(0,10);
+    var ten_sample_labels = data.otu_labels.slice(0,10);
+    var ten_sample_values = data.sample_values.slice(0,10);
 
     var ten_sample_data = [{
       "labels" : ten_sample_ids,
       "values" : ten_sample_values,
       "hovertext" : ten_sample_labels,
       "type" : "pie"
-  }];
-
-  Plotly.newPlot('pie', ten_sample_data);
-  })
-};
+  }]
+var data2 = [ten_sample_data];
+  Plotly.newPlot('pie', data2, chart_layout);
+  });
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
